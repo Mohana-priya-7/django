c5c3dfe28a6b456e1a32764cba56fad832666b7f
Mohana-priya-7 <s.mohanapriya2174@gmail.com>
2026-01-08 17:47:33 +0530

    Django Project

--- products/models.py
+++ products/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 class Product(models.Model):
-    name = models.CharField(max_length=255)
-    Description = models.TextField()
-    Price = models.DecimalField(max_digits=10, decimal_places=2)
+    Name = models.CharField(max_length=255)
+    Price = models.IntegerField()
+    Description = models.CharField(max_length=1000)
     created_at = models.DateTimeField(auto_now_add=True)
     def __str__(self):
-        return self.name
+        return self.Name
