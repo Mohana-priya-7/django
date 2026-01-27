db1b899e7d56effac34b4c91a78c43a34ef2bd28
Mohana-priya-7 <s.mohanapriya2174@gmail.com>
2026-01-27 12:14:57 +0530

    This is project

--- products/models.py
+++ products/models.py
@@ -9,9 +9,9 @@ class Product(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return self.name
-
 class ForgetPassword(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    """ This allows the ForgetPassword model to track which user requested a password reset. The CASCADE behavior ensures no orphaned records remain if a user account is removed."""
     otp = models.CharField(max_length=6)
     created_at = models.DateTimeField(default=timezone.now)
     is_used = models.BooleanField(default=False)
